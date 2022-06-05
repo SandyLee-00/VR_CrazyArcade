01e670f13a9cc56496780c6eb08e64abf802427e
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-06-05 16:34:56 +0900

    Add Prefabs as basic objects

--- IMP_CrazyArcade/Assets/Scripts/Seoyeong_Scripts/XVR_FPSController.cs
+++ IMP_CrazyArcade/Assets/Scripts/Seoyeong_Scripts/XVR_FPSController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 
-public class SC_FPSController : MonoBehaviour
+public class XVR_FPSController : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
