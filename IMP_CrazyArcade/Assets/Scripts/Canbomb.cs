c86a29fbc2e3663133b2e3bbee970ce42d702e95
gaeunU <dbrkdmsyge12@naver.com>
2022-06-08 02:29:26 +0900

    test enemy and maze bomb
    test enemy and maze bomb -> fail

---
+++ IMP_CrazyArcade/Assets/Scripts/Canbomb.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canbomb : MonoBehaviour
+{
+    public AudioClip collidesound;
+    AudioSource audiosource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audiosource.clip = collidesound;
+        audiosource.Play();
+        Destroy(other);
+    }
+}
