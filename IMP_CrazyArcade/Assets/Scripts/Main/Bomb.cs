668a9fef68b98bd69608b4070df23425fcd93767
gaeunU <dbrkdmsyge12@naver.com>
2022-06-12 03:08:03 +0900

    Add OVR Manager & Button A,B input
    Add OVR Manager package
    
    if get down A or B -> bubble active false or true

--- IMP_CrazyArcade/Assets/Scripts/Main/Bomb.cs
+++ IMP_CrazyArcade/Assets/Scripts/Main/Bomb.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    GameObject temp;
     public AudioClip collidesound;
     AudioSource audiosource;
-    
+    public GameObject BubbleBomb;
+    public GameObject Hand;
+    Vector3 pos;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +21,22 @@ public class Bomb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (OVRInput.Get(OVRInput.RawButton.A))
+        {
+            // pos = Hand.transform.position;
+            // temp = Instantiate(BubbleBomb, new Vector3(pos.x, -0.41f, pos.z), Quaternion.identity);
+            BubbleBomb.SetActive(false);
+        }else if (OVRInput.Get(OVRInput.RawButton.B))
+        {
+            // pos = Hand.transform.position;
+            // temp = Instantiate(BubbleBomb, new Vector3(pos.x, -0.41f, pos.z), Quaternion.identity);
+            BubbleBomb.SetActive(true);
+        }
+
+
     }
 
-    
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -29,8 +45,20 @@ public class Bomb : MonoBehaviour
             audiosource.clip = collidesound;
             audiosource.Play();
             Destroy(other.gameObject);
+
         }
     }
 
-    
+    void PressButton()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.A))
+        {
+            // setActive Bubble
+          //  pos = Hand.transform.position;
+           // temp = Instantiate(BubbleBomb, new Vector3(pos.x, -0.41f, pos.z), Quaternion.identity);
+            BubbleBomb.SetActive(!BubbleBomb);
+        }
+    }
+
+
 }
